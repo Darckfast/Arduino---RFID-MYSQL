4b9a88cc8585c0abfa9cb4977e9873e07a2596c9
Darckfast <victorcosta37@gmail.com>
2018-09-09 22:35:53 -0300

    Código finalizado

--- src/rfidquery.ino
+++ src/rfidquery.ino
@@ -1,28 +1,4 @@
 /*
-  MySQL Connector/Arduino Example : query results
-
-  This example demonstrates how to issue a SELECT query and how to read columns
-  and rows from the result set. Study this example until you are familiar with how to
-  do this before writing your own sketch to read and consume query results.
-
-  NOTICE: You must download and install the World sample database to run
-          this sketch unaltered. See http://dev.mysql.com/doc/index-other.html.
-
-  INSTRUCTIONS FOR USE
-
-  1) Change the address of the server to the IP address of the MySQL server
-  2) Change the user and password to a valid MySQL user and password
-  3) Connect a USB cable to your Arduino
-  4) Select the correct board and port
-  5) Compile and upload the sketch to your Arduino
-  6) Once uploaded, open Serial Monitor (use 115200 speed) and observe
-
-  Note: The MAC address can be anything so long as it is unique on your network.
-
-  Created by: Dr. Charles A. Bell
-*/
-
-/*
   Esquema de cores do LED
   BRANCO -> Iniciando o arduino
   AZUL -> Conexão feita com sucesso
@@ -39,6 +15,8 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#include <SpritzCipher.h>
+
 #define COMMON_ANODE
 #define redPin 5
 #define greenPin 6
@@ -53,12 +31,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
 
 byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
-IPAddress server_addr(192, 168, 1, 45); // IP of the MySQL *server* here
-//char user[] = "admin";              // MySQL user login username
-//char password[] = "1234";        // MySQL user login password
-
-// query para selecionar o banco
-//char querySelectDB[] = "USE arduino";
+IPAddress server_addr(192, 168, 1, 44); // IP of the MySQL *server* here
 
 EthernetClient client;
 MySQL_Connection conn((Client *)&client);
@@ -79,51 +52,51 @@ void setup() {
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 
-  //byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 
   Ethernet.begin(mac_addr);
   Serial.println(F("Connecting..."));
 
-  //Chama a função que vai conectar com o database MySQL
   conn_led();
-
+  Serial.println(F("Aguardando Leitura"));
 }
 void loop() {
   led_off();
   bool acess = false;
+ 
+  char select[75];
+  char insert[100];
 
-  //Query que vai buscar o cartão no banco de dados
-  String q = "SELECT * FROM ID WHERE id = '?'";
-  q.reserve(40);
-  // Look for new cards
   if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    Serial.print(F("."));
     return;
   }
-
-  // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) {
+    Serial.print(F("."));
     return;
   }
 
-  //Substitui '?' pela ID do cartão
-  q.replace("?", getID());
+  String hashCARD = hash();
+  
+  sprintf(select,"SELECT idCARD FROM CARD WHERE hash = '%s';", hashCARD.c_str());
+  
+  char ac = '0';
 
-  //Serial.println(q);
+  Serial.println();
+  
+  Serial.println(hashCARD);
+
   Serial.println(F("\nRunning SELECT and printing results\n"));
 
   // Initiate the query class instance
   MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
-
+  Serial.println("Conn");
   const char* querySelectDB = "USE arduino";
 
   cur_mem->execute(querySelectDB);
 
-  //Converte string para char*
-  const char *query = q.c_str();
-  Serial.println(query);
+  Serial.println(select);
 
-  //Executa a query
-  cur_mem->execute(query);
+  cur_mem->execute(select);
 
   //Pega as colunas
   column_names *cols = cur_mem->get_columns();
@@ -164,57 +137,31 @@ void loop() {
       led_off();  // OFF red
 
     } else {
-      //setColor(0, 255, 0);  // ON green
       Serial.println(F("Identificação encontrada"));
-      /*if(acess){
+      if (acess) {
+        ac = '1';
         green();
-        digitalWrite(relayPin,HIGH);
+        //digitalWrite(relayPin, HIGH);
         delay(2000);
         led_off();  // OFF green
-        digitalWrite(relayPin,LOW);
-        }else{
+        //digitalWrite(relayPin, LOW);
+      } else {
         red();  // ON red
-        Serial.println("Identificação sem acesso");
+        Serial.println("Identificação com acesso insuficiente");
         delay(2000);
         led_off();  // OFF red
-        }*/
-      switch (acess) {
-        case true:
-          green();
-          //COM-NC
-          digitalWrite(relayPin, HIGH);
-          delay(2000);
-          led_off();  // OFF green
-          digitalWrite(relayPin, LOW);
-        default:
-          red();  // ON red
-          Serial.println(F("Identificação sem acesso"));
-          delay(2000);
-          led_off();  // OFF red
       }
     }
     count++;
   } while (row != NULL);
 
-  // Deleting the cursor also frees up memory used
+  sprintf(insert, "INSERT INTO LOGS (hashCARD, data, granted) VALUES ('%s', NOW(), %c);", hashCARD.c_str(), ac);
+
+  cur_mem->execute(insert);
   delete cur_mem;
+  Serial.println(F("Aguardando Leitura"));
 }
-String getID() {
-
-  String tagID = "";
-
-  for (uint8_t i = 0; i < 4; i++) {
-    readCard[i] = mfrc522.uid.uidByte[i];
-    tagID.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-
-  tagID.toUpperCase();
-  mfrc522.PICC_HaltA();
-
-  return tagID;
-}
-void setColor(int red, int green, int blue)
-{
+void setColor(int red, int green, int blue){
 #ifdef COMMON_ANODE
   red = 255 - red;
   green = 255 - green;
@@ -243,6 +190,32 @@ void conn_led() {
   delay(1000);
   led_off();
 }
+
+String hash()
+{
+  byte data[4];
+  
+  for (byte i = 0; i < 4; i++) {
+    data[i] = mfrc522.uid.uidByte[i];
+  }
+  
+  mfrc522.PICC_HaltA();
+  
+  byte hashLen = 16;
+  byte digest[hashLen];
+
+  unsigned int i;
+  
+  spritz_hash(digest, hashLen, data, 4);
+
+  String hash;
+  
+  for ( i = 0; i < sizeof(digest);i++){
+    hash.concat(String(digest[i], HEX));
+  }
+  
+  return hash;
+}
 void red () {
   setColor(255, 0, 0);
 }
@@ -258,6 +231,10 @@ void yellow() {
 void white() {
   setColor(255, 255, 255);
 }
+void purple() {
+  setColor(255, 0, 255);
+}
 void led_off() {
   setColor(0, 0, 0);
 }
+
